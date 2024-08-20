3c503035325a154af8215defd03bf74abd79572e
Aaron <ahabyari@students.uni-mainz.de>
2024-08-21 00:11:02 +0200

    createdatabase

--- create_database.sql
+++ create_database.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Users (
-    user_id INT PRIMARY KEY,
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     acc_creation_date DATE DEFAULT CURRENT_DATE,
